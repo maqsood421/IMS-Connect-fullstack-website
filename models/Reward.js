4aa21dbd8f7a98694d78e66c35e2822aad23d841
Maqsood Ahmed <104073101+maqsood421@users.noreply.github.com>
2024-11-24 01:08:51 +0500

    Delete models directory

--- models/Reward.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const RewardSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    points: { type: Number, default: 0 },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Reward', RewardSchema);
--- models/Vote.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const VoteSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    idea_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Idea', required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Vote', VoteSchema);
