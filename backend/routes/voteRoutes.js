852392f7e3cf2596eccd8e71998405a93a48d903
Maqsood Ahmed <104073101+maqsood421@users.noreply.github.com>
2024-11-25 16:52:32 +0500

    Delete backend directory

--- backend/routes/voteRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { castVote, getVotesByIdea } = require('../controllers/voteController');
-const authMiddleware = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.post('/', authMiddleware, castVote);        // Cast a vote
-router.get('/:ideaId', authMiddleware, getVotesByIdea); // Get votes for an idea
-
-module.exports = router;
